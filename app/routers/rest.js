b60164eb5018be753318825635a52e309cba86bd
Eric W. Greene <eric@training4developers.com>
2015-09-09 20:20:29 -0600

    tweaked rest service

--- app/routers/rest.js
+++ app/routers/rest.js
@@ -3,75 +3,44 @@ module.exports = function(fileName) {
 	"use strict";
 
 	var
-		//mongoose = require("mongoose"),
 		modelName = require("camelcase")(fileName),
 		pluralize = require('mongoose/lib/utils').toCollectionName,
 		collectionName = 	pluralize(modelName),
 		DataModel = require("../models/" + fileName),
-		express = require("express"),
-		router = express.Router();
+		router = require("express").Router();
+
+	function handleResult() {
+		var res = this;
+		return function(err, results) {
+			if (err) {
+				global.logger.error(err);
+				res.status(500).end();
+				return;
+			}
+			res.json(results);
+		}
+	}
 
 	router.route("/" + collectionName)
 		.get(function(req, res) {
-
-			DataModel.find({}, function(err, results) {
-				if (err) {
-					global.logger.error(err);
-					res.status(500).json(err);
-					return;
-				}
-				res.json(results);
-			});
+			DataModel.find({}, handleResult.call(res));
 		});
 
 	router.route("/" + collectionName)
 		.post(function(req, res) {
-
 			var t = new DataModel(req.body);
-			t.save(function(err, result) {
-				if (err) {
-					global.logger.error(err);
-					res.status(500).json(err);
-					return;
-				}
-				res.json(result);
-			});
+			t.save(handleResult.call(res));
 		});
 
 	router.route("/" + collectionName + "/:id")
 		.get(function(req, res) {
-			DataModel.findById(req.params.id,
-				function(err, result) {
-					if (err) {
-						global.logger.error(err);
-						res.status(500).json(err);
-						return;
-					}
-					res.json(result);
-				});
+			DataModel.findById(req.params.id, handleResult.call(res));
 		})
 		.put(function(req, res) {
-			DataModel.findByIdAndUpdate(req.params.id,
-				req.body,
-				function(err, result) {
-					if (err) {
-						global.logger.error(err);
-						res.status(500).json(err);
-						return;
-					}
-					res.json(req.body);
-				});
+			DataModel.findByIdAndUpdate(req.params.id, req.body, handleResult.call(res));
 		})
 		.delete(function(req, res) {
-			DataModel.findByIdAndRemove(req.params.id,
-				function(err, result) {
-					if (err) {
-						global.logger.error(err);
-						res.status(500).json(err);
-						return;
-					}
-					res.json(result);
-				});
+			DataModel.findByIdAndRemove(req.params.id, handleResult.call(res));
 		});
 
  	return router;
